94ad8f12520e66512ae3bf07bcb17f25dfdd1684
fvrrrn <boris.chernystrand@gmail.com>
2024-05-16 20:15:22 +0300

    initial commit

---
+++ .lintstagedrc.js
@@ -0,0 +1,11 @@
+const path = require('path')
+
+const buildEslintCommand = (filenames) =>
+  `next lint --max-warnings=0 --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`
+
+module.exports = {
+  '*.{js,jsx,ts,tsx,md,html,css,scss}': 'prettier --write',
+  '!(.lintstagedrc)*.{js,jsx,ts,tsx}': [buildEslintCommand]
+}
